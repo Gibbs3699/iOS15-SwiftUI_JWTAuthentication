50e0f58e58b2cd93ab0725f900e1c089abaa58c2
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-05-11 22:37:59 +0700

    Initial Commit

---
+++ iOS15-SwiftUI-JWTAuthentication/iOS15_SwiftUI_JWTAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS15_SwiftUI_JWTAuthenticationApp.swift
+//  iOS15-SwiftUI-JWTAuthentication
+//
+//  Created by TheGIZzz on 11/5/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS15_SwiftUI_JWTAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
